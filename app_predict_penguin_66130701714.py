baa50dcca57e155e8b3140e5f858072cc6f4dc4e
DitsagornT <161188354+DitsagornT@users.noreply.github.com>
2024-11-10 13:47:49 +0700

    Update app_predict_penguin_66130701714.py

--- app_predict_penguin_66130701714.py
+++ app_predict_penguin_66130701714.py
@@ -53,4 +53,4 @@ x_new['sex'] = sex_encoder.transform(x_new['sex'])
 # Button to make the prediction
 if st.button('Predict'):
     species = predict_penguin(x_new.iloc[0])  # Pass the first row of the DataFrame
-    st.write(f"The predicted species is: {species[0]}")
+    st.write(f"The predicted species is: {species}")
